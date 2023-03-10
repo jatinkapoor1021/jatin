87f21c83fe244486f5ca9bf3e093899d11ab1f34
Jatin Kapoor <jatinkapoor1021@gmail.com>
2023-03-09 19:48:46 -0500

    updating

--- tests.cpp
+++ tests.cpp
@@ -8,12 +8,12 @@
 TEST_CASE("TESTS"){
     CHECK(shiftChar('a', 25) == 'z');
     CHECK(shiftChar('z', 25) == 'y');
-    CHECK(encryptCaesar("Hello, World!", 10) == "Rovvy, Gybvn!");
-    CHECK(encryptCaesar("What a sentence", 10) == "Grkd k coxdoxmo");
-    CHECK(decryptCaesar("Rovvy, Gybvn!", 10) == "Hello, World!");
-    CHECK(decryptCaesar("Grkd k coxdoxmo", 10) == "What a sentence");
-    CHECK(encryptVigenere("Hello, World!", "cake") == "Jevpq, Wyvnd!");
-    CHECK(encryptVigenere("What a sentence", "power") == "Lvwx r hsjxvcqa");
-    CHECK(decryptVigenere("Jevpq, Wyvnd!", "cake") == "Hello, World!");
-    CHECK(decryptVigenere("Lvwx r hsjxvcqa", "power") == "What a sentence");
+    CHECK(encryptCaesar("Hello, how are you!", 10) == "Stgffe, sta frp yui!");
+    CHECK(encryptCaesar("What are you doing?", 10) == "Hyio tui pou iytdse?");
+    CHECK(decryptCaesar("Rovvy, Gybvn!", 10) == "Hello, how are you!");
+    CHECK(decryptCaesar("Grkd k coxdoxmo", 10) == "What are you doing?");
+    CHECK(encryptVigenere("Hello my friends!", "cake") == "Jrtuv hj uioprve!");
+    CHECK(encryptVigenere("What is the plan for this weekend!", "power") == "Hruj ip typ efhk ipe onvc yjuqwel");
+    CHECK(decryptVigenere("Jrtuv hj uioprve!", "cake") == "Hello my friends!");
+    CHECK(decryptVigenere("Hruj ip typ efhk ipe onvc yjuqwel", "power") == "What is the plan for this weekend!");
 }

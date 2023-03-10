8abc8f430fba2095d629ed96bea30acc4c43f59a
Jatin Kapoor <jatinkapoor1021@gmail.com>
2023-03-09 19:28:13 -0500

    updating

--- main.cpp
+++ main.cpp
@@ -11,16 +11,16 @@ int main(){
     std::cout<<shiftChar('z', 25)<<"\n";
 
     std::cout<<encryptCaesar("Hello, how are you!", 10)<<"\n";
-    std::cout<<encryptCaesar("What are you doing", 10)<<"\n";
+    std::cout<<encryptCaesar("What are you doing?", 10)<<"\n";
 
-    std::cout<<decryptCaesar("Rovvy, Gybvn!", 10)<<"\n";
-    std::cout<<decryptCaesar("Grkd k coxdoxmo", 10)<<"\n";
+    std::cout<<decryptCaesar("Stgffe, sta frp yui!", 10)<<"\n";
+    std::cout<<decryptCaesar("Hyio tui pou iytdse?", 10)<<"\n";
 
     std::cout<<encryptVigenere("Hello my friends!", "cake")<<"\n";
-    std::cout<<encryptVigenere("What is the plan for this weekend", "power")<<"\n";
+    std::cout<<encryptVigenere("What is the plan for this weekend!", "power")<<"\n";
 
-    std::cout<<decryptVigenere("Jevpq, Wyvnd!", "cake")<<"\n";
-    std::cout<<decryptVigenere("Lvwx r hsjxvcqa", "power")<<"\n";
+    std::cout<<decryptVigenere("Jrtuv hj uioprve!", "cake")<<"\n";
+    std::cout<<decryptVigenere("Hruj ip typ efhk ipe onvc yjuqwel", "power")<<"\n";
     
     
 }
